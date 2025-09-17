cce6dbcfe5a20c025335675d8bb02857573889de
Steopa2001 <moraru495@gmail.com>
2025-09-17 19:04:53 +0200

    feat: create the db connection

--- data/db.js
+++ data/db.js
@@ -1,2 +1,10 @@
 // importo mysql2
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+
+// creo la connessione al db 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_blog',
+})
